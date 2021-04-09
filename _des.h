aa56dfa66c92d13e779b5f08133409baf9d43355
Kivanc Yazan <kyzn@cpan.org>
2021-04-10 01:48:28 +0900

    Apply patch at https://rt.cpan.org/Public/Bug/Display.html?id=133363

--- _des.h
+++ _des.h
@@ -4,4 +4,6 @@ typedef unsigned long des_ks[32];
 
 void _des_crypt( des_cblock in, des_cblock out, des_ks key, int encrypt );
 void _des_expand_key( des_user_key userKey, des_ks key );
+void perl_des_expand_key(des_user_key userKey, des_ks ks);
+void perl_des_crypt( des_cblock input, des_cblock output, des_ks ks, int encrypt );
 
